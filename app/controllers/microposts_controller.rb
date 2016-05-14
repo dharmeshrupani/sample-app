7f14ca4348da27c648c8eb666ae77e9ad69b27a8
Dharmesh Rupani <dR@Dharmeshs-MacBook-Pro.local>
2016-05-14 15:54:31 -0700

    Add 'Edit post' module

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,5 +1,5 @@
 class MicropostsController < ApplicationController
-  before_action :logged_in_user, only: [:create, :destroy]
+  before_action :logged_in_user, only: [:create, :destroy, :edit, :update]
   before_action :correct_user,   only: :destroy
 
   def create
@@ -12,6 +12,17 @@ class MicropostsController < ApplicationController
       render 'static_pages/home'
     end
   end
+
+  def edit  
+     @micropost = Micropost.find(params[:id])
+  end
+
+  def update  
+    @micropost = Micropost.find(params[:id])
+    @micropost.update(micropost_params)
+
+    redirect_to root_url
+  end 
 
   def destroy
     @micropost.destroy
